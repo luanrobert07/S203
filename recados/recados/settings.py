e5abcf705375382f1089ffd866d82d3a8ee85cec
luanrobert07 <luan.robert@gec.inatel.br>
2024-02-20 20:42:15 -0300

    Configuração do banco de dados

--- recados/recados/settings.py
+++ recados/recados/settings.py
@@ -74,10 +74,14 @@ WSGI_APPLICATION = 'recados.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+ 'default': {
+ 'ENGINE': 'django.db.backends.postgresql',
+ 'HOST': 'localhost',
+ 'NAME': 'recadosdb',
+ 'PORT': 5432,
+ 'USER': 'postgres',
+ 'PASSWORD': 'password'
+ }
 }
 
 
